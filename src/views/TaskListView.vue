3dbaf9b828c2cb3f33fad4c974476127f4695281
Irina <sidoren-irina@yandex.ru>
2023-06-23 11:48:06 +0300

    Fix add empty task on enter & make task empty while edit

--- src/views/TaskListView.vue
+++ src/views/TaskListView.vue
@@ -3,13 +3,13 @@
     <h1>Список задач</h1>
     <div class="flex align-items-center gap-2">
       <div class="flex flex-column gap-2 relative">
-        <InputText class="task-list__input" id="add-task" v-model="addTodoValue" aria-describedby="add-task-help"
-                   @keyup.enter="addTask(addTodoValue)" autocomplete="none" />
-        <span class="absolute left-0 top-100 text-base text-red-500 py-1" v-show="store.getters.getTodoByName(addTodoValue)"
+        <InputText class="task-list__input" id="add-task" v-model="newTodoValue" aria-describedby="add-task-help"
+                   @keyup.enter="addTask(newTodoValue)" autocomplete="none" />
+        <span class="absolute left-0 top-100 text-base text-red-500 py-1" v-show="store.getters.getTodoByName(newTodoValue) && newTodoValue !== ''"
               id="add-task-help">Такая задача уже есть</span>
       </div>
       <Button class="task-list__button" type="button" label="Добавить" severity="success" icon="pi pi-plus"
-              @click="addTask(addTodoValue)" :disabled="addTodoValue === '' || store.getters.getTodoByName(addTodoValue)" />
+              @click="addTask(newTodoValue)" :disabled="newTodoValue === '' || store.getters.getTodoByName(newTodoValue)" />
     </div>
     <div class="mt-5">
       <div v-for="todo of todos" :key="todo.key" class="flex align-items-center justify-content-between gap-4 mb-3">
@@ -17,7 +17,11 @@
           <Checkbox v-model="selectedTodos" :inputId="todo.key" name="todo" :value="todo.name" @input="toggleDone(todo)" />
           <label v-if="!todo.isEdit" :for="todo.key">{{ todo.name }}</label>
           <div v-else class="flex align-items-center gap-2 w-full">
-            <InputText class="w-full" type="text" v-model="todo.name" @input="todo.done = false" @keyup.enter="editTask(todo)" v-focus />
+            <div class="flex flex-column gap-2 relative w-full">
+              <InputText type="text" v-model="todo.name" @input="todo.done = false" @keyup.enter="editTask(todo)" v-focus />
+              <span class="absolute left-0 top-100 text-base text-red-500 py-1" v-show="todo.name === ''"
+                    id="add-task-help">Задача не может быть пустой</span>
+            </div>
             <Button class="flex-shrink-0" icon="pi pi-check" aria-label="Отредактировано" severity="success" @click="editTask(todo)" />
           </div>
         </div>
@@ -40,7 +44,7 @@
 import { Todo } from "../types/todo";
 
   const todos = store.state.todos
-  const addTodoValue = ref<string>('')
+  const newTodoValue = ref<string>('')
   const selectedTodos = ref<Array<string>>([])
 
   const { saveToLocalTodos } = useLocalStorage()
@@ -55,9 +59,9 @@ import { Todo } from "../types/todo";
   })
 
   const addTask = (value: string) => {
-    if (!store.getters.getTodoByName(value)) {
+    if (!store.getters.getTodoByName(value) && value !== '') {
       store.commit('addTodo', value)
-      addTodoValue.value = ''
+      newTodoValue.value = ''
     }
   }
 
@@ -74,8 +78,11 @@ import { Todo } from "../types/todo";
   }
 
   const editTask = (todo: Todo) => {
-    todo.isEdit = false
-    store.commit('editTodo', todo)
+    if (todo.name !== '') {
+      console.log('name', todo.name)
+      todo.isEdit = false
+      store.commit('editTodo', todo)
+    }
   }
 
 </script>
